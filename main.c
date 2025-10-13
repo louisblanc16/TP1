77efcb6b1d12eba99de421b4378b9cacb9a164ef
Louis BLANC <louis.blanc@student.junia.com>
2025-10-13 13:55:33 +0200

    Exercice 1

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello");
+    int seconds;
+    printf("Entrez un nombre de secondes : ");
+    scanf("%d", &seconds);
+    int hours = seconds / 3600;
+    int minutes = (seconds % 3600) / 60;
+    int remaining_seconds = seconds % 60;
+    printf("%d heures, %d minutes et %d secondes\n", hours, minutes, remaining_seconds);
+
     return 0;
 }
