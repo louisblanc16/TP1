684f9f7b0070d905c8017ace80762a3e438e52a1
Louis BLANC <louis.blanc@student.junia.com>
2025-10-13 15:34:34 +0200

    Exercice 3

--- main.c
+++ main.c
@@ -1,20 +1,58 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main() {
-    float C, t, M;  
-    int n;
+    const char *pendu[] = {
+        "\n\n\n\n\n\n\n-------\n",
+        "\n |\n |\n |\n |\n |\n |\n-------\n",
+        " -------\n | |\n |\n |\n |\n |\n-------\n",
+        " -------\n | |\n | O\n |\n |\n |\n-------\n",
+        " -------\n | |\n | O\n | |\n |\n |\n-------\n",
+        " -------\n | |\n | O\n | /|\\\n |\n |\n-------\n",
+        " -------\n | |\n | O\n | /|\\\n | / \\\n |\n-------\n"
+    };
 
-    printf("Montant du pret (C) : ");
-    scanf("%f", &C);
-    printf("Taux d'interet annuel (en pourcentage) : ");
-    scanf("%f", &t);
-    printf("Duree du pret (en annees) : ");
-    scanf("%d", &n);
+    char motSecret[50], motAffiche[50], lettre;
+    int erreurs = 0, i, trouve;
 
-    M = (C * (t / 12)) / (1 - pow(1 + (t / 12), -n * 12));
+    printf("Entre le mot secret (sans espaces) : ");
+    scanf("%s", motSecret);
 
-    printf("La mensualite du pret est : %.2f euros\n", M);
+    int longueur = strlen(motSecret);
+    for (i = 0; i < longueur; i++) motAffiche[i] = '_';
+    motAffiche[longueur] = '\0';
+
+    while (erreurs < 6 && strcmp(motSecret, motAffiche) != 0) {
+        printf("\nMot : %s\n", motAffiche);
+        printf("Lettre : ");
+        scanf(" %c", &lettre);
+        lettre = tolower(lettre);
+
+        trouve = 0;
+        for (i = 0; i < longueur; i++) {
+            if (tolower(motSecret[i]) == lettre && motAffiche[i] == '_') {
+                motAffiche[i] = motSecret[i];
+                trouve = 1;
+            }
+        }
+
+        if (!trouve) {
+            erreurs++;
+            printf("%s\n", pendu[erreurs]);
+        }
+
+        printf("\n");
+    }
+
+    if (strcmp(motSecret, motAffiche) == 0)
+        printf("Bravo ! t'as trouvé le mot : %s\n", motSecret);
+    else
+        printf("Perdu ! le mot était : %s\n", motSecret);
 
     return 0;
 }
