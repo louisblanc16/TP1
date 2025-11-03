5b2b985baa3a98491b50c850fb9b814b2fdad1bc
Louis BLANC <louis.blanc@student.junia.com>
2025-11-03 14:28:33 +0100

    correction compil8

---
+++ compil8.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 5;
+    printf("x = %d\n", y); //error: 'y' undeclared (first use in this function) -> la variable y n'a pas été déclarée
+    return 0;
+}
