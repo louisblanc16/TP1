e538493cd374e0f09444a3d9e4ed32e17faaa895
Louis BLANC <louis.blanc@student.junia.com>
2025-11-03 14:22:33 +0100

    correction compil6

---
+++ compil6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    if (x == 5) { //il faut mettre un double égal pour comparer deux valeurs
+        printf("x vaut 5\n");
+    }
+    return 0;
+}
