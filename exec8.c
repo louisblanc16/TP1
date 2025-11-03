98f6214f1755efd502d067902de5036089b79374
Louis BLANC <louis.blanc@student.junia.com>
2025-11-03 15:14:29 +0100

    correction exec8

---
+++ exec8.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int t[3] = {10, 20, 30};
+    int i = 0; //Il faut initialiser i à 0 pour commencer à l'indice 0 donc au début du tableau
+    printf("%d\n", t[i]); 
+    return 0;
+}
