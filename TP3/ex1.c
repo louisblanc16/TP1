0041c1615fe122681c9546c174c87a6960a55be1
Louis BLANC <louis.blanc@student.junia.com>
2025-11-17 14:11:16 +0100

    etape4

--- TP3/ex1.c
+++ TP3/ex1.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 
-int main () {
+int main() {
     int n;
-    int width, max_height;
+    int width, max_height, mid;
     char img[100][100];
     printf("Entrez une taille n entre 5 et 10 : ");
     scanf("%d", &n);
-    if (n>=5 && n<=10) {
+    if (n >= 5 && n <= 10) {
         width = n * n;
         max_height = 3 * n - 1;
-        for (int i=0; i<width; i++) {
-            for (int j=0; j<max_height; j++) {
+        mid = width / 2;
+        for (int i = 0; i < max_height; i++) {
+            for (int j = 0; j < width; j++) {
                 img[i][j] = ' ';
             }
         }
-        for (int i=0; i<width; i++) {
-            for (int j=0; j<max_height; j++) {
-                printf("%c",img[i][j]);
+        for (int i = 0; i < n; i++) {
+            for (int j = mid - i; j <= mid + i; j++) {
+                if (j == mid - i || j == mid + i) {
+                    img[i][j] = 'A';
+                } else {
+                    img[i][j] = 'S';
+                }
+            }
+            if (i == n - 1) {
+                img[i][mid - i - 1] = '_';
+                img[i][mid + i + 1] = '_';
+            }
+        }
+        for (int i = 0; i < max_height; i++) {
+            for (int j = 0; j < width; j++) {
+                printf("%c", img[i][j]);
             }
             printf("\n");
         }
-    return 0;
     }
+    return 0;
 }
