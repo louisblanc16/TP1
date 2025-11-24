2b73671bc3c3464c2c64c8cae604983d3a240413
Louis BLANC <louis.blanc@student.junia.com>
2025-11-24 12:55:30 +0100

    etape1

---
+++ TP4/tp4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void afficherMenu() {
+    printf("===== GESTION DE NOTES =====\n");
+    printf("1. Saisir le nombre d'eleves\n");
+    printf("2. Saisir les notes des eleves\n");
+    printf("3. Afficher toutes les notes\n");
+    printf("4. Afficher la moyenne d'un eleve\n");
+    printf("5. Afficher la moyenne generale\n");
+    printf("6. Afficher la meilleure note de chaque controle\n");
+    printf("0. Quitter\n");
+}
+
+int main() {
+    afficherMenu();
+    return 0;
+}
