8bb40ce7bb7dbed2632b19a7754dabccfd835877
Louis BLANC <louis.blanc@student.junia.com>
2025-11-27 11:32:09 +0100

    etape4

--- TP4/ex2.c
+++ TP4/ex2.c
@@ -23,6 +23,21 @@ int montant_valide(int montant) {
     return 1;
 }
 
+void calcul_distribution(int montant) {
+    int b50 = montant / 50;
+    montant = montant % 50;
+    int b20 = montant / 20;
+    montant = montant % 20;
+    int b10 = montant / 10;
+    montant = montant % 10;
+    int b5  = montant / 5;
+    printf("Billets distribues :\n");
+    printf("%d billets de 50 euros\n", b50);
+    printf("%d billets de 20 euros\n", b20);
+    printf("%d billets de 10 euros\n", b10);
+    printf("%d billets de 5 euros\n", b5);
+}
+
 int main() {
     int choix = 0;
     while (choix != 2) {
@@ -37,6 +52,9 @@ int main() {
             if (montant_valide(montant) == 0) {
                 printf("Erreur : montant invalide.\n");
                 printf("Le montant doit etre entre 5 euros et 1000 euros, divisible par 5.\n");
+            }
+            else {
+                calcul_distribution(montant);
             }
         }
         else {
