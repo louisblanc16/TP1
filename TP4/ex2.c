3338ab38595b2b8131d182f30a7e5d746de7db8b
Louis BLANC <louis.blanc@student.junia.com>
2025-11-27 11:13:39 +0100

    etape2

--- TP4/ex2.c
+++ TP4/ex2.c
@@ -6,6 +6,13 @@ void afficher_menu() {
     printf("2 - Quitter\n");
 }
 
+int saisir_montant() {
+    int montant;
+    printf("Montant a retirer : ");
+    scanf("%d", &montant);
+    return montant;
+}
+
 int main() {
     int choix = 0;
     while (choix != 2) {
@@ -14,6 +21,9 @@ int main() {
         scanf("%d", &choix);
         if (choix == 2) {
             printf("Au revoir.\n");
+        }
+        else if (choix == 1) {
+            int montant = saisir_montant();
         }
         else {
             printf("Choix invalide.\n");
